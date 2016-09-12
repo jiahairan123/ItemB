d5843b649737083a23afa48645eebe111bf5260f
贾海然 <915386240@qq.com>
2016-09-12 13:14:26 +0800

    贾海然Normal

---
+++ app/src/main/java/com/example/dllo/itemb/NormalJia.java
@@ -0,0 +1,15 @@
+package com.example.dllo.itemb;
+
+/**
+ * Coder: JiaHaiRan
+ * created on 16/9/12 13:13
+ */
+
+public class NormalJia {
+
+    int name;
+
+    public NormalJia(int name) {
+        this.name = name;
+    }
+}
