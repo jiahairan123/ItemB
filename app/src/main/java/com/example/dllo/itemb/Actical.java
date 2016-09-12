fbc301d4e77d8737f78389810e8cc4b4ea2d9447
dllo <dllo@dllodeMac-mini-151.local>
2016-09-12 11:39:52 +0800

    第一天

---
+++ app/src/main/java/com/example/dllo/itemb/Actical.java
@@ -0,0 +1,31 @@
+package com.example.dllo.itemb;
+
+/**
+ * Created by dllo on 16/9/12.
+ */
+public class Actical {
+
+    private int a;
+    private int b;
+
+    public Actical(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
