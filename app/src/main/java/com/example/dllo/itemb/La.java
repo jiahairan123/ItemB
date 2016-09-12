5e31d1ea60000fea20954657450d3bdf46c585df
dllo <dllo@dllodeMac-mini-151.local>
2016-09-12 11:49:13 +0800

    第二次

---
+++ app/src/main/java/com/example/dllo/itemb/La.java
@@ -0,0 +1,26 @@
+package com.example.dllo.itemb;
+
+/**
+ * Created by dllo on 16/9/12.
+ */
+public class La {
+
+    private int c;
+    private int d;
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+}
