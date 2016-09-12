3aad270a5e288bfd7716e76075db774c3fae8cc1
dllo <dllo@dllodeMac-mini-86.local>
2016-09-12 11:29:51 +0800

    no message

--- app/src/main/java/com/example/dllo/itemb/MainActivity.java
+++ app/src/main/java/com/example/dllo/itemb/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int abc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ceshi 
+
+        //ceshi
+        //贾海然夫妻欺负人
     }
 }
